86dbd896ce059e080062961c12a586e795ed974a
Han,  DongGyun <DongGyun.Han@rhul.ac.uk>
2024-10-16 12:29:35 +0100

    #3 update pom.xml (#4)

--- LectureDemo/src/main/java/uk/ac/ucl/comp0010/model/Movie.java
+++ LectureDemo/src/main/java/uk/ac/ucl/comp0010/model/Movie.java
@@ -3,6 +3,9 @@ package uk.ac.ucl.comp0010.model;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A class represents a movie.
+ */
 public class Movie {
 
   List<Integer> rates;
@@ -16,6 +19,12 @@ public class Movie {
     this.rates.add(rate);
   }
 
+  /**
+   * Returns an average rate of a moive.
+   *
+   * @return a double value of the average rate.
+   * @throws EmptyRateException when there is no rate.
+   */
   public Double getAverageRate() throws EmptyRateException {
     if (rates.size() < 1) {
       throw new EmptyRateException();
